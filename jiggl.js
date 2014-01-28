1c9d2d6eaea1d1f3a396f3ddeb07ef68f7432a7d
chrisrickard <chris.rickard@gmail.com>
2014-01-29 10:22:11 +1100

    Persisting toggl against Jira task

--- jiggl.js
+++ jiggl.js
@@ -31,8 +31,7 @@ var app = {
 		set_toggl_id: function(issue, toggl_id) {
 			var path = ('/rest/api/2/issue/' + issue);
 			var data = {fields: {}};
-			data.fields[config.jira_toggl_field_id] = toggl_id;
-
+			data.fields[config.jira_toggl_field_id] = (toggl_id).toString();
 			return this.put(path, {data: JSON.stringify(data), headers:{'content-type': 'application/json'}});
 		}
 	}),
@@ -60,9 +59,8 @@ var app = {
 
 		this.get_jira_issues()
 			.then(this.create_toggl_tasks)
-			.fail(function(e){
-				console.log('[Error] ', e);
-			})
+			.then(this.save_toggl_ids_in_jira)
+			.fail(function(e){ console.log('[Error] ', e); })
 			.done(this.toggl_import_complete);
 	},
 
@@ -81,31 +79,67 @@ var app = {
 	},
 
 	create_toggl_tasks: function(issues) {
-		return Q.allSettled(issues.map(function(issue){
-			var task_name = [issue.key, ' - ', issue.fields.summary].join('');
-			return app.create_toggl_task(task_name);
-		}));
+		var deferred = Q.defer();
+
+		Q.allSettled(issues.map(function(issue) {
+			return app.create_toggl_task(issue);
+		})).then(function(promises) {
+			var created_tasks = [];
+			promises.forEach(function(promise){
+				if(promise.state === 'fulfilled') {
+					created_tasks.push(promise.value);
+				}
+			});
+			deferred.resolve(created_tasks);
+		});
+
+		return deferred.promise;
 	},
 
-	create_toggl_task: function(name) {
+	create_toggl_task: function(issue) {
+		var name = [issue.key, ' - ', issue.fields.summary].join('');
+
 		var deferred = Q.defer();
 		this.toggl_service.create_task(config.toggl_project_id, name).on('complete', function(data, response) {
 			if(response.statusCode ===  200) {
 				var task = data.data;
 				console.log('[Created] Toggl task ' + task.id + ' From Jira issue ' + name);
-
-				//save jira toggl id...
-				//this.jira_service.set_toggl_id('JIG-3', "12345").on('complete', function(data, response){
-				//	console.log(data, response.statusCode);
-				//});
-
-				deferred.resolve(task);
+				deferred.resolve({ issue: issue, task: task});
 			}
 			else {
 				console.log('[Error] Toggl task (' + name + ') could not be created: ' + JSON.stringify(data));
 				deferred.reject();
 			}
 		});
+
+		return deferred.promise;
+	},
+
+	save_toggl_ids_in_jira: function(tasks) {
+		return Q.allSettled(tasks.map(function(task) {
+			return app.save_toggl_id_in_jira(task.issue, task.task);
+		}));
+
+		//save jira toggl id...
+		//this.jira_service.set_toggl_id('JIG-3', "12345").on('complete', function(data, response){
+		//	console.log(data, response.statusCode);
+		//});
+	},
+
+	save_toggl_id_in_jira: function(issue, task) {
+		var deferred = Q.defer();
+		this.jira_service.set_toggl_id(issue.key, task.id).on('complete', function(data, response) {
+
+			if(response.statusCode === 204) {
+				console.log('[Saved] Toggl ID saved to Jira Issue ' + issue.key);
+				deferred.resolve(task);
+			}
+			else {
+				deferred.reject(new Error('Jira issues could not be collected: ' + JSON.stringify(data)));
+				console.log('[Error] Toggl id (' + task.id + ') could not be saved to Jira issue ' + issue.key + ': ' + JSON.stringify(data));
+			}
+		});
+
 		return deferred.promise;
 	},
 
